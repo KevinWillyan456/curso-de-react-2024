a1229460f1adb09e8ed19507f8d157fdbe7a42f9
Kevin Souza <kevinwillyansouza@gmail.com>
2024-11-30 11:05:47 -0400

    feat: implementar persistência de tarefas no localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { v4 as uuid } from 'uuid'
 import AddTask from './components/AddTask'
 import Tasks from './components/Tasks'
-import { v4 as uuid } from 'uuid'
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: 'Estudar Programação',
-            description: 'Estudar programação para ficar fera',
-            isCompleted: false,
-        },
-        {
-            id: 2,
-            title: 'Ler Livros',
-            description: 'Ler livros para adquirir conhecimento',
-            isCompleted: false,
-        },
-        {
-            id: 3,
-            title: 'Fazer Exercícios',
-            description: 'Fazer exercícios para ficar saudável',
-            isCompleted: false,
-        },
-    ])
+    const [tasks, setTasks] = useState(
+        JSON.parse(localStorage.getItem('tasks')) || []
+    )
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, [tasks])
 
     function onTaskClick(taskId) {
         const newTasks = tasks.map((task) => {
